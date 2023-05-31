944ce5521b5ae5ccc00e9c412e46e76da01e9020
danl062 <127430829+danl062@users.noreply.github.com>
2023-05-31 22:43:35 +0300

    Create FinalFlask.py

---
+++ FinalFlask.py
@@ -0,0 +1,50 @@
+from io import BytesIO
+
+from flask import Flask, request, jsonify
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+from keras.applications.resnet50 import preprocess_input
+from PIL import Image
+import requests
+
+
+app = Flask(__name__)
+
+model = load_model('/Users/danlellouche/Downloads/my_model.h5')
+
+# Dictionnaire des labels
+labels_dict = {0: 'default', 1: 'fire', 2: 'smoke'}
+
+@app.route('/', methods=['GET'])
+def home():
+    """
+    Home endpoint of the Churn Prediction API.
+    """
+    return "Welcome to the Hackathon Fire Detection API! Use the /predict_fire?image_url= route to make predictions."
+
+@app.route('/predict_fire', methods=['GET'])
+def predict():
+    image_url = request.args.get('image_url')
+
+    # Téléchargez l'image à partir de l'URL
+    response = requests.get(image_url)
+    img = Image.open(BytesIO(response.content))
+
+    img = img.resize((224, 224))
+    img_array = img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+
+    pred_vec = model.predict(img_array)
+    pred_class = np.argmax(pred_vec)
+    confidence = np.max(pred_vec)
+    label = labels_dict[pred_class]
+
+    result = {'label': label, 'confidence': float(confidence)}
+    print(str(result))
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run()
